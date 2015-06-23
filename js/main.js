890eb104a39c2d23c42d41194e23e22332e698cf
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2015-06-23 15:26:24 +0200

    only compressed theme

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  //$('.bullet-after').after('<li class="menuBull">•</li>');
-  var bullets = $('.bullet-after');
-  for (i = 0; i < bullets.length; i++) {
-    console.log(bullets[i]);
-      //bullets[i].after('<li class="menuBull">•</li>');
-  }
-});
